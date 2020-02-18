ef2539d58124621358e801d3f3d78f91d08130c5
jpb <jpb@guitaresphere.com>
2020-02-18 19:37:42 +0100

    update .htaccess for distant behavior

--- config.php
+++ config.php
@@ -18,9 +18,10 @@ $app = new \Slim\App($settings);
 use \Psr\Http\Message\ServerRequestInterface as Request;
 use \Psr\Http\Message\ResponseInterface as Response;
 
-$app->group('/v1', function () use ($app) {
+$app->group('/api/v1', function () use ($app) {
 
     $app->get('/', function (Request $request, Response $response, Array $args) {
+
         return $response->withJson("entering v1");
     });
 
