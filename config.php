d7b43ebe9e0eb2b84362cfc9609eb0d86aa37d33
jpb <jpb@guitaresphere.com>
2020-02-18 20:19:28 +0100

    add Access-Control-Allow-Methods

--- config.php
+++ config.php
@@ -18,9 +18,34 @@ $app = new \Slim\App($settings);
 use \Psr\Http\Message\ServerRequestInterface as Request;
 use \Psr\Http\Message\ResponseInterface as Response;
 
-$app->group('/api/v1', function () use ($app) {
+// This is the middleware
+// It will add the Access-Control-Allow-Methods header to every request
 
-    $app->get('/', function (Request $request, Response $response, Array $args) {
+$app->add(function($request, $response, $next) {
+    $route = $request->getAttribute("route");
+    $methods = [];
+    if (!empty($route)) {
+        $pattern = $route->getPattern();
+        foreach ($this->router->getRoutes() as $route) {
+            if ($pattern === $route->getPattern()) {
+                $methods = array_merge_recursive($methods, $route->getMethods());
+            }
+        }
+        //Methods holds all of the HTTP Verbs that a particular route handles.
+    } else {
+        $methods[] = $request->getMethod();
+    }
+
+    $response = $next($request, $response);
+    return $response->withHeader("Access-Control-Allow-Methods", implode(",", $methods));
+});
+
+
+$app->group('/api/v1/', function () use ($app) {
+
+    $app->get('', function (Request $request, Response $response, Array $args) {
+
+        var_dump($request->getHeader('api_user_token'));
 
         return $response->withJson("entering v1");
     });
